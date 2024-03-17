fd21653fd17c6754ab884b03140af4cab38951b0
choi8803 <choijh225@naver.com>
2024-03-17 16:25:25 +0900

    complete

--- src/index.js
+++ src/index.js
@@ -2,16 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import rootReducer, { rootSaga } from './modules';
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
+// import myLogger from './middlewares/myLogger';
+import logger from 'redux-logger';
+import { thunk } from 'redux-thunk';
+import { Router, BrowserRouter } from 'react-router-dom';
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+// 1.Redux DevTools 사용하기
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(rootReducer, applyMiddleware(myLogger, logger));  // 여러개의 미들웨이 적용가능
+const store = createStore(
+  rootReducer, 
+  composeEnhancers(
+    // logger를 사용하는 경우 logger가 가장 마지막에 위치 해야된다.
+    applyMiddleware(
+      thunk,
+      sagaMiddleware,
+      logger    // 2.redux-logger 사용하기
+    )   
+  )
+); 
+
+sagaMiddleware.run(rootSaga);   // 스토어 생성이 된 다음에 실행되어야 한다.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
